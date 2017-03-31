40ea0e5ed34dc246ea09ba3cc15d252404f488c4
Glauter Santos <glauterlima@gmail.com>
2017-03-31 09:43:41 -0300

    aula 6999 neri front end

--- fontes/front-end/controller/cursosjson.js
+++ fontes/front-end/controller/cursosjson.js
@@ -1,11 +1,9 @@
-{
-	"cursos":
-	[
-		{"codigo":"1", "descricao":"Java", "diashorarios":"Segunda, 18hs", "professor":"Raul Santos"},
-		{"codigo":"2", "descricao":"PHP", "diashorarios":"Terça, 18hs", "professor":"Rosimery"},
-		{"codigo":"3", "descricao":"Python", "diashorarios":"Quarta, 18hs", "professor":"Nicanor Vieira"},
-		{"codigo":"4", "descricao":"Arduino", "diashorarios":"Quinta, 18hs", "professor":"Neri"},
-		{"codigo":"4", "descricao":"WebDesigner", "diashorarios":"Sexta 18hs", "professor":"Lucia de Fatima"},
+var cursoModulo = angular.module('cursoModulo',[]);
 
-	]
-}
+cursoModulo.controller("cursoController", function ($scope, $http) {
+
+	$http.get('controller/cursosjson.json').then(function(response) {
+		$scope.cursos = response.data.cursos;
+	})	
+	
+});
